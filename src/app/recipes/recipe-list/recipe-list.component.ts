670a9ac4793165a6f943fc5fc07e90d6a5748d30
jhonas.alves <jhonas.alves@myorgtech.com>
2022-08-06 12:35:38 -0300

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Teste Recipe',
+      'This is simply a test',
+      'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*'
+    ),
+    new Recipe(
+      'A Teste Recipe',
+      'This is simply a test',
+      'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
